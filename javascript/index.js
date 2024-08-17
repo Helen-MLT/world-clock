32f1a51f5d290ae4a407506d69378e13fc580b39
Helen-MLT <h.milletti@gmail.com>
2024-08-17 12:28:40 +0200

    Added link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -40,7 +40,8 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small></div>
-        </div>`;
+        </div>
+        <a href="/">All cities</a>`;
 }
 updateTime();
 setInterval(updateTime, 1000);
